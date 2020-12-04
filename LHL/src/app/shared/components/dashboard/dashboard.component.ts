abd1c918e33618027a86f4ccc6c8571542e04292
Itish <itish.singh1@bp.com>
2020-12-04 18:22:16 +0530

    pushing fixes

--- LHL/src/app/shared/components/dashboard/dashboard.component.ts
+++ LHL/src/app/shared/components/dashboard/dashboard.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { TextData } from '../../modals/text-modal'
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  textData = TextData;
+  constructor() { 
 
-  constructor() { }
+  }
+  onAboutUs(){
+    console.log('ok')
+  }
 
   ngOnInit(): void {
   }
---
+++ LHL/src/app/shared/modals/text-modal.ts
@@ -0,0 +1,11 @@
+export class TextData {
+    static dashboard = {
+        title: 'Specifications :',
+        line1: 'Easily configurable via Web-App, So no download needed just fire-it-up',
+        line2: 'More than 60 pre-defined transitions to play with',
+        line3: 'Elegant Mood Light that uses high powered, 16 million colour LEDs, to fit in your mood',
+        line4: 'Compact in size',
+        line5: 'Plug & Play',
+        line6: 'Integrate with your Social platform to get notified in different colour',
+    }
+}
